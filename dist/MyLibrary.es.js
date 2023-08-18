29f5bc101f06021c8fd85583c5689e14c6353d8e
達偉 吳 <qqw741852963tw@gmail.com>
2023-08-18 15:28:08 +0800

    init

--- dist/MyLibrary.es.js
+++ dist/MyLibrary.es.js
@@ -1,21 +1,21 @@
-import { ref as c, openBlock as l, createElementBlock as r, toDisplayString as s } from "vue";
-const a = (e, o) => {
-  const t = e.__vccOpts || e;
-  for (const [n, _] of o)
-    t[n] = _;
-  return t;
+import { ref as _, openBlock as l, createElementBlock as r, toDisplayString as s } from "vue";
+const a = (t, e) => {
+  const o = t.__vccOpts || t;
+  for (const [c, n] of e)
+    o[c] = n;
+  return o;
 }, p = {
   __name: "HelloWorld",
-  setup(e) {
-    const o = c(0), t = () => {
-      o.value++;
+  setup(t) {
+    const e = _(0), o = () => {
+      e.value++;
     };
-    return (n, _) => (l(), r("button", {
+    return (c, n) => (l(), r("button", {
       type: "button",
-      onClick: t
-    }, "count is " + s(o.value), 1));
+      onClick: o
+    }, "count is " + s(e.value), 1));
   }
-}, d = /* @__PURE__ */ a(p, [["__scopeId", "data-v-4a27bb39"]]);
+}, d = /* @__PURE__ */ a(p, [["__scopeId", "data-v-8afefce5"]]);
 export {
   d as HelloWord
 };
